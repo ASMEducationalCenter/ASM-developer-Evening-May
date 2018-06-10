8325d16879d49ac0f7c47ae1e0f45dec0527b7ce
Saurav Sharma <sauravsharma011@gmail.com>
2018-06-10 18:39:23 -0400

    Update createTable.py

--- DynamoDB/createTable.py
+++ DynamoDB/createTable.py
@@ -1,25 +1,30 @@
 import boto3
 
-client = boto3.client('dynamodb')
-
-response = client.create_table(
-TableName='test-table1',
-#this is the attribute keys and type
-AttributeDefinitions=[
-        {
-        'AttributeName': 'name',
-        'AttributeType': 'S'
-        }
-    ],
-#This is the primary key
-KeySchema=[
-        {
-        'AttributeName': 'name',
-        'KeyType': 'HASH'
-        }
-    ],
-        ,
-    ProvisionedThroughput={
-        'ReadCapacityUnits': 1,
-        'WriteCapacityUnits': 1
-)
+def lambda_handler(event, context):
+ 
+    
+    
+    client = boto3.client('dynamodb')
+    
+    response = client.create_table(
+    TableName='test-table1',
+    #this is the attribute keys and type
+    AttributeDefinitions=[
+            {
+            'AttributeName': 'name',
+            'AttributeType': 'S'
+            }
+        ],
+    #This is the primary key
+    KeySchema=[
+            {
+            'AttributeName': 'name',
+            'KeyType': 'HASH'
+            }
+        ],
+            
+        ProvisionedThroughput={
+            'ReadCapacityUnits': 1,
+            'WriteCapacityUnits': 1
+            }
+    )
