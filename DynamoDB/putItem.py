af3a0938a920fbaca25a48d040f9c3d836ed4c6c
Saurav Sharma <sauravsharma011@gmail.com>
2018-06-10 18:44:41 -0400

    Update putItem.py

--- DynamoDB/putItem.py
+++ DynamoDB/putItem.py
@@ -1,16 +1,16 @@
 import boto3
+def lambda_handler(event, context):
+        client = boto3.resource('dynamodb')
 
-client = boto3.resource('dynamodb')
-
-table = client.Table('test-table1')
-response= table.put_item( Item={
-        'name': 'Sanjog',
-        'title': 'Paralegal',
-        'info': {
-            'plot':"Nothing happens at all.",
-            'rating': 10
-        }
-    })
+        table = client.Table('test-table1')
+        response= table.put_item( Item={
+                'name': 'Sanjog',
+                'title': 'Paralegal',
+                'info': {
+                    'plot':"Nothing happens at all.",
+                    'rating': 10
+                }
+            })
 
 
-print(response["Items"][0])
+        print(response["Items"][0])
